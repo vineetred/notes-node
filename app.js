85a34c1d0a5837d25d4a3dda513f1a584b77a9c9
Vineet Reddy <saivineetreddy.thatiparthi_ug19@ashoka.edu.in>
2017-12-13 23:09:00 +0530

    Removed few lines of bs

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
-
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
-var filteredArray  = _.uniq(['Andrew', 3, 'Andrew', 1,2,3,4]);
-console.log(filteredArray);
-
-
-// var user  = os.userInfo();
-// fs.appendFileSync('greetings.txt',`Hello ${user.username}! You are ${notes.sex}`);
-// var res = notes.addNote();
-// console.log(res);
-// var addition = notes.add(10,27);
-// notes.add(9,-2);
+//User written modules
+const notes = require('./notes.js');
