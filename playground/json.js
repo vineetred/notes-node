3dfacc6f9bf3a6661cb16e523a1fc35b0ca4628d
Vineet Reddy <saivineetreddy.thatiparthi_ug19@ashoka.edu.in>
2017-12-19 19:30:39 +0530

    JSON changes

--- playground/json.js
+++ playground/json.js
@@ -16,9 +16,9 @@ var originalNote  = {
     body: 'Some Body',
 };
 var originalNoteString = JSON.stringify(originalNote);
- fs.writeFileSync('notes.json',originalNoteString);
+fs.writeFileSync('notes.json',originalNoteString);
 
- var noteString = fs.readFileSync('notes.json');
+var noteString = fs.readFileSync('notes.json');
 var note = JSON.parse(noteString);
 
  console.log(typeof(note));
