ea4178732bd3ff4cdc2a587facf540f8bdf21a0e
Vineet Reddy <saivineetreddy.thatiparthi_ug19@ashoka.edu.in>
2017-12-19 19:27:29 +0530

    Playing with JSON!

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command:', command);
 // console.log('Process', process.argv);
 console.log('Yargs', argv);
---
+++ playground/json.js
@@ -0,0 +1,25 @@
+// var obj = {
+//     name: 'Vineet',
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Vineet", "age": "20"}';
+// var person = JSON.parse(personString);
+// // console.log(typeof(person));
+
+const fs = require('fs');
+
+var originalNote  = {
+    title: 'Some Title',
+    body: 'Some Body',
+};
+var originalNoteString = JSON.stringify(originalNote);
+ fs.writeFileSync('notes.json',originalNoteString);
+
+ var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+ console.log(typeof(note));
+ console.log(note.title);
