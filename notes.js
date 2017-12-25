34b0c9978d533bc6d2c4a6bca331061aec9ec878
Vineet Reddy <hotshot2797@gmail.com>
2017-12-25 17:06:56 +0530

    Made the 'Add' functionality

--- notes.js
+++ notes.js
@@ -1,7 +1,20 @@
 console.log("Starting notes.js!");
-
+var fs = require('fs');
 var addNote = (title, body) => {
-    console.log("Adding Note: ", title, body);
+    var notes = [];
+    var note = {
+        title,
+        body,
+    };
+    //This basically reads existing notes and parses it
+    //into the empty notes array
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+    //This pushes the note body and title into notes-data.json file
+    notes.push(note);
+    // Writing into the file
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
 };
 
 var getAll = () => {
