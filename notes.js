10ce01c654ec93e1ce20dae65a0320b07d8e75c5
Vineet Reddy <hotshot2797@gmail.com>
2017-12-25 17:22:44 +0530

    Added functionality to check for duplicate notes

--- notes.js
+++ notes.js
@@ -6,14 +6,25 @@ var addNote = (title, body) => {
         title,
         body,
     };
-    //This basically reads existing notes and parses it
-    //into the empty notes array
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-    //This pushes the note body and title into notes-data.json file
-    notes.push(note);
-    // Writing into the file
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    //The code below exists in case notes-data.json does not
+    try {
+        //This basically reads existing notes and parses it
+        //into the empty notes array
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+    //Checks if title is duplicate
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        //This pushes the note body and title into notes-data.json file
+        notes.push(note);
+        // Writing into the file
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
 
 };
 
